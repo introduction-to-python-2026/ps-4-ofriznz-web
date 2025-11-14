b9ae3c75d473400a473c4696bea71480ab62d288
ofriznz-web <ofriznz@gmail.com>
2025-11-14 19:20:27 +0200

    Update string_utils.py 😀

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,25 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+    split_formula = []
+    start = 0
+    for end in range(1, len(formula)):
+        if formula[end].isupper():
+           split_formula.append(formula[start:end])
+           start = end 
+            
+    if formula:  
+       split_formula.append(formula[start:len(formula)])  
+  
+    return split_formula
+    
+    
 
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+    digit_location = 1
+    for char in formula[1:]:
+        if char.isdigit():
+            break
+        digit_location += 1
+    if digit_location == len(formula):
+        return (formula, 1)
+    return (formula[:digit_location], int(formula[digit_location:]))
